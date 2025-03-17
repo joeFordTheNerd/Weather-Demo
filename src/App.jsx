d720fb741a08b047a9d6a14d267cc466285c9c1a
joeFordTheNerd <joeforduk@gmail.com>
2025-03-17 21:25:20 +0000

    feat: add geolocation fetch from user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react"
 import Card from "./components/Card/Card"
 import CardContainer from "./components/CardContainer/CardContainer"
 import SearchBar from "./components/SearchBar/SearchBar"
@@ -6,14 +7,33 @@ import Container from "./components/Container/Container"
 
 function App() {
 
-const onSearch = (query) => {
-  console.log(query)
-  // fetch data from the API
-  // update the state
-  // render the data in the Card component
-}
+  // get lat long from entered town or city
+  const fetchLatLong = async (place) => {
+    try {
+      const response = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(place)}&count=1`
+      );
+      const data = await response.json();
 
+      if (data.results && data.results.length > 0) {
+        const { latitude, longitude } = data.results[0];
+        return { latitude, longitude }
+      }else{
+        console.log("No results found")
+      }
 
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  const onSearch = async (query) => {
+    const location = await fetchLatLong(query)
+    if (location) {
+      console.log(`Lat/Long:`, location.latitude, location.longitude)
+    }
+  }
 
 
   return (
