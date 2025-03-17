b70494518c7b8b6ad7219fe40f27202b1cf7cc99
joeFordTheNerd <joeforduk@gmail.com>
2025-03-17 18:31:01 +0000

    feat: add button to trigger search

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,29 +1,32 @@
 import React, { useState } from "react";
 import * as styles from './SearchBar.module.css'
 
-function SearchBar({onSearch}) {
+function SearchBar({ onSearch }) {
   const [query, setQuery] = useState("")
-  
+
   const handleChange = (e) => {
     setQuery(e.target.value)
   }
 
+  // nice to have in addition to submit button
   const handleEnter = (e) => {
-    if(e.key === 'Enter') {
+    if (e.key === 'Enter') {
       onSearch(query)
     }
   }
 
   return (
-    <div className={styles.searchBar}>
-        <input 
-          type="text" 
+    <>
+      <div className={styles.searchBar}>
+        <input
+          type="text"
           placeholder="Enter a location..."
           onChange={handleChange}
           onKeyDown={handleEnter}
-          />
-    </div>
-    // todo - add a submit button and make it look nice
+        />
+      <button onClick={() => onSearch(query)}>Search</button>
+      </div>
+    </>
   )
 }
 
