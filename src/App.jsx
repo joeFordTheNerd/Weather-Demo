18976cbadaa37ad188fc934c1ca1f026748d4ed0
joeFordTheNerd <joeforduk@gmail.com>
2025-03-17 22:21:47 +0000

    feat: fetch and format weather data

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,34 @@ import Container from "./components/Container/Container"
 
 function App() {
 
+  const fetchWeather = async (latitude, longitude) => {
+    try {
+      const response = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min,wind_speed_10m_max&timezone=auto&forecast_days=5`
+      );
+      const weatherData = await response.json();
+      if (weatherData.daily) {
+        //get the data we want for the weather cards and return a clean object
+        const formattedWeather = weatherData.daily.time.map((date, index) => ({
+          date: date,
+          weatherCode: weatherData.daily.weather_code[index],
+          maxTemp: weatherData.daily.temperature_2m_max[index],
+          minTemp: weatherData.daily.temperature_2m_min[index],
+          windSpeed: weatherData.daily.wind_speed_10m_max[index]
+        }));
+
+        console.log(formattedWeather)
+
+      } else {
+        console.log("No results found")
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
   // get lat long from entered town or city
   const fetchLatLong = async (place) => {
     try {
@@ -18,7 +46,7 @@ function App() {
       if (data.results && data.results.length > 0) {
         const { latitude, longitude } = data.results[0];
         return { latitude, longitude }
-      }else{
+      } else {
         console.log("No results found")
       }
 
@@ -31,13 +59,12 @@ function App() {
   const onSearch = async (query) => {
     const location = await fetchLatLong(query)
     if (location) {
-      console.log(`Lat/Long:`, location.latitude, location.longitude)
+      fetchWeather(location.latitude, location.longitude)
+
     }
   }
 
-
   return (
-
     <Container>
       <Heading />
       <SearchBar onSearch={onSearch} />
